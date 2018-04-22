3443ea8576dfe08976c07762de8b69c1d7f22998
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2018-04-23 04:19:45 +0800

    Create PostOrderGetHeight.c

---
+++ PostOrderGetHeight.c
@@ -0,0 +1,11 @@
+int PostOrderGetHeight(BinTree BT){
+  int HL, HR, MaxH;
+  if(BT){
+    HL = PostOrderGetHeight(BT->Left);
+    HR = PostOrderGetHeight(BT->Right);
+    MaxH = (HL > HR) ? HL : HR;
+    return (MaxH + 1);
+  }
+  else
+    return 0;
+}
