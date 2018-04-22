b44675b2f1a87852ef713e0952ba04afaa1e1679
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2018-04-23 04:04:14 +0800

    Update InOrderTraversal.c

--- InOrderTraversal.c
+++ InOrderTraversal.c
@@ -1,1 +1,17 @@
-void()
+void InOrderTraversal(BinTree BT){
+  BinTree T = BT;
+  Stack S = CreateStack(MaxSize);
+  while(T || IsEmpty(S)){
+    while(T){
+      Push(S,T);
+      T = T->left;
+    }
+    if(!IsEmpty(S)){
+      T = Pop(S);
+      printf("%d", T->data);
+      T = T->right;
+    }
+  }
+}
+      
+  
