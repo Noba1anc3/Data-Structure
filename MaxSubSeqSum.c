1e7556cac5918069987ce1d38c83e775aa8568a7
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2018-04-23 03:38:54 +0800

    Update and rename main.py to MaxSubSeqSum.c

---
+++ MaxSubSeqSum.c
@@ -0,0 +1,13 @@
+int MaxSubseqSum( int A[], int N){
+    int ThisSum, MaxSum;
+    int i;
+    ThisSum = MaxSum = 0;
+    for(i = 0;i < N;i++){
+        ThisSum += A[i];
+        if(ThisSum > MaxSum)
+            MaxSum = ThisSum;
+        else if(ThisSum < 0)
+            ThisSum = 0;
+    }
+    return MaxMum;
+}
