b194f4f590640410474de4b5cf00ef8eaf9e1083
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2018-04-23 09:51:38 +0900

    Create InsertInBSTree.c

---
+++ InsertInBSTree.c
@@ -0,0 +1,14 @@
+BinTree InsertInBSTree(ElementType X, BinTree BST){
+  if(!BST){
+    BST = malloc(sizeof(struct TreeNode));
+    BST->Data = X;
+    BST->Left = BST->Right = NULL;
+  }
+  else{
+    if(X < BST->Data)
+      BST->Left = InsertInBSTree(X,BST->Left);
+    else
+      BST->Right = InsertInBSTree(X,BST->Right);
+  }
+  return BST;
+}
