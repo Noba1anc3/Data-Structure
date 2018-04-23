af52ad21ee6bdbbf3e2817c184041d684e93f5d1
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2018-04-23 10:07:44 +0900

    Update DeleteInBSTree.c

--- DeleteInBSTree.c
+++ DeleteInBSTree.c
@@ -4,8 +4,23 @@ BST DeleteInBSTree(ElementType X, BinTree BST){
     printf("None");
   else if( X < BST->Data)
     BST->Data = Delete(X,BST->Left);
-  else if( X < BST->Data)
+  else if( X > BST->Data)
     BST->Data = Delete(X,BST->Right);
   else{
-    if(BST->Left && BST->Right)
+    if(BST->Left && BST->Right){
+        Tmp = FindMin(BST->Right);
+        BST->Data = Tmp->Data;
+        BST->Right = Delete(BST->Data,BST->Right);
+    }
+    else{
+      Tmp = BST;
+      if(!BST->Left)
+        BST = BST->Right;
+      else if(!BST->Right)
+        BST = BST->Left;
+      free(Tmp);
+    }
+  }
+  return BST;
+}
     
