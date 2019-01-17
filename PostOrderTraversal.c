acb7f7c57f9c48960c67714093ae3354113bcf80
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-01-17 22:59:02 +0800

    Update PostOrderTraversal.c

--- PostOrderTraversal.c
+++ PostOrderTraversal.c
@@ -4,33 +4,23 @@ void PostOrderTraversal(BinTree BT){
   int S_temp[Maxsize], flag, top = -1;
 
   while(T || top != -1){
-
     while(T){
-
       S[++top] = T;
       S_temp[top] = 0; 
       T = T->left;
-
     }
-
+    
     T = S[top];
     flag = s_temp[top--];
     
     if(flag == 0){
-
       S[++top] = T;
       S_temp[top] = 1; 
       T = T->right;
-
     }
-    
     else{
-      
       VISIT(T);
       T = NULL;
-      
     }
-
   }
-
 }
