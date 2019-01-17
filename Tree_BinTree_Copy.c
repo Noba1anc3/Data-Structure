3e0657ab499ecbc767f74a932c8aafcf3e597797
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-01-18 00:18:32 +0800

    Update Tree_BinTree_Copy.c

--- Tree_BinTree_Copy.c
+++ Tree_BinTree_Copy.c
@@ -3,7 +3,7 @@ BTree COPYBT(BTree T1){
   if(T1 == NULL)
     return NULL;
   else{
-    T2 = (BTree)malloc(sixeof(BTNode));
+    T2 = (BTree)malloc(sizeof(BTNode));
     T2->data = T1->data;
     T2->lchild = COPYBT(T1->lchild);
     T2->rchild = COPYBT(T1->rchild);
