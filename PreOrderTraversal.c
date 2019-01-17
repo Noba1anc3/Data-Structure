c8eb7afaeb6a2a6216359676d8bf5b7423a00bd1
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-01-17 22:59:53 +0800

    Update PreOrderTraversal.c

--- PreOrderTraversal.c
+++ PreOrderTraversal.c
@@ -1,13 +1,13 @@
 void PreOrderTraversal(BinTree BT){
   BinTree T = BT;
   Stack S = CreateStack(MaxSize);
-  while(T || IsEmpty(S)){
+  while(T || NotEmpty(S)){
     while(T){
       Push(S,T);
       printf("%d", T->data);
       T = T->left;
     }
-    if(!IsEmpty(S)){
+    if(!Empty(S)){
       T = Pop(S);
       T = T->right;
     }
