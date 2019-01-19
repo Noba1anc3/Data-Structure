d889e85c79f535a9c72c10bf2c50688fd7698c33
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-01-19 13:11:18 +0800

    Update and rename DeleteInBSTree.c to Tree_BinSortTree_Delete.c

--- Tree_BinSortTree_Delete.c
+++ Tree_BinSortTree_Delete.c
@@ -1,18 +1,18 @@
-BinTree DeleteInBSTree(ElementType X, BinTree BST){
-  Position Tmp;
+BinTree DeleteInBSTree(ElementType X, BTree BST){
+  BTNode Tmp;
   if(!BST)
     printf("None");
   else if( X < BST->Data)
-    BST->Left = DeleteInBSTree(X,BST->Left);
+    BST->Left = DeleteInBSTree(X, BST->Left);
   else if( X > BST->Data)
-    BST->Right = DeleteInBSTree(X,BST->Right);
+    BST->Right = DeleteInBSTree(X, BST->Right);
   else{
     if(BST->Left && BST->Right){
         Tmp = FindMin(BST->Right);
-      //Tmp = FindMax(BST->Left);
+      //Tmp = FindMax(BST->Left );
         BST->Data = Tmp->Data;
-        BST->Right = DeleteInBSTree(BST->Data,BST->Right);
-      //BST->Left = DeleteInBSTree(BST->Data,BST->Left);
+        BST->Right = DeleteInBSTree(BST->Data, BST->Right);
+      //BST->Left  = DeleteInBSTree(BST->Data, BST->Left );
     }
     else{
       Tmp = BST;
