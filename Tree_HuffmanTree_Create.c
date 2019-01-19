34b444ddc86c7f1ecfce07d7611a659148e84579
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-01-19 13:57:36 +0800

    Update and rename HuffmanTree.c to Tree_HuffmanTree_Create.c

--- Tree_HuffmanTree_Create.c
+++ Tree_HuffmanTree_Create.c
@@ -1,21 +1,18 @@
-typedef struct TreeNode *HuffmanTree;
 struct TreeNode{
   int Weight;
-  HuffmanTree Left,Right;
-}
+  TreeNode *Left, *Right;
+}TreeNode, *HuffmanTree;
 
-HuffmanTree HuffmanTree( MinHeap H){
+HuffmanTree CreateHuffmanTree(MinHeap H){
   int i;
   HuffmanTree T;
-  for(i = 1;i<H->Size;i++){
+  for(i = 1;i < H->Size;i++){
     T = malloc(sizeof(struct TreeNode));
     T->Left = DeleteInMinHeap(H);
     T->Right = DeleteInMinHeap(H);
     T->Weight = T->Left->Weight + T->Right->Weight;
-    Insert(H,T);
+    Insert(H, T);
   }
   T = DeleteInMinHeap(H);
   return T;
 }
-
-//O(NlogN)
