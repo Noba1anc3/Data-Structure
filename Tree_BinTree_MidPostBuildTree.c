9c2a1a5c1a2762fcf060c5af9b17ca94495eeb2f
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-02-25 23:09:49 +0800

    Update Tree_BinTree_MidPostBuildTree.c

--- Tree_BinTree_MidPostBuildTree.c
+++ Tree_BinTree_MidPostBuildTree.c
@@ -1,30 +1,51 @@
-BinTree ReBuildTree(char *post, int i, int j, char *in, int m, int n)
+BinTree ReBuildTree(int *post, int i, int j, int *in, int m, int n)
+
 {
+
     //post[i.....j] in[m....n]
+
     int x;  //x是遍历器
+
     BinTree p;   //p是新的结点
+
     p = (BinTree)malloc(sizeof(BinTNode));
+
     p->data = *(post + j);      //从后序序列中读取结点信息
+
     x = m;
 
-    //在中序中找到根节点位置
-    for (; *(in + x) != *(post + j); x++)
-        ;
+
+
+	//在中序中找到根节点位置
+
+    for (; *(in + x) != p->data; x++)
+
+		;
+
+
 
     //接下来递归完成左右子树的构建
+
     if (x == m) 
-        p->lchild = NULL;
+
+		p->lchild = NULL;
+
     else
-    {
+
         p->lchild = ReBuildTree(post, i, i+x-m-1, in, m, x-1);
-    }
+
+
 
     if (x == n) 
-	p->rchild = NULL;
+
+		p->rchild = NULL;
 
     else
-    {
-        p->rchild = ReBuildTree(post, i+x-m, j-1, in, x+1,n);
-    } 
+
+        p->rchild = ReBuildTree(post, i+x-m, j-1, in, x+1, n);
+
+ 
+
     return p;
+
 }
