80f34d2256a37d47d90c28cd3676aa585c679688
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-02-25 23:06:43 +0800

    Update Tree_BinTree_Traversal_LevelOrder.c

--- Tree_BinTree_Traversal_LevelOrder.c
+++ Tree_BinTree_Traversal_LevelOrder.c
@@ -14,3 +14,36 @@ void LevelOrderTraversal(BinTree BT){
       AddQ(Q, T->Right);
   }
 }
+
+///////////////////////////////////////////////////////////
+
+void LevelOrderTraverse(int head, BTNode nodes[]){
+
+	int queue[10];
+	int front = 0, rear = 0;
+
+	if(head < 0)
+		return;
+
+	queue[rear++] = head;
+
+	while(front != rear){
+		int prt = queue[front++];
+
+		if( count < n-1 ){
+			printf("%d ", prt);
+			count++;
+		}
+		else
+			printf("%d\n", prt);
+
+		if(nodes[prt].lchild >= 0)
+			queue[rear++] = nodes[prt].lchild;
+		
+		if(nodes[prt].rchild >= 0)
+			queue[rear++] = nodes[prt].rchild;
+	}
+
+	count = 0;
+
+}
