538863dc702304a9adb6716fc733acc4051203a9
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-03-16 14:38:14 +0800

    Create Recursive_Queens.java

---
+++ Recursive_Queens.java
@@ -0,0 +1,14 @@
+public static void findQueen(int row){
+	if(row > 7){
+		total++;
+		PrintQueen();
+		return;
+	}
+	for(int column = 0; column < 8; column++){
+		if(check(row, column)){
+			array[row][column] = 1;
+			findQueue(row+1);
+			array[row][column] = 0;
+    }
+  }
+}
