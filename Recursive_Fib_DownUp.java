2dd19139df011b2a0970e5e4a2bdfe701d63d720
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-03-16 15:15:14 +0800

    Update and rename Recursive_Fib_DownUpjava to Recursive_Fib_DownUp.java

--- Recursive_Fib_DownUp.java
+++ Recursive_Fib_DownUp.java
@@ -6,8 +6,7 @@ public static int fib(int n)
         memo[0]=0;
         memo[1]=1;
         for(int i = 2; i <= n; i++)
-        {
-            memo[i] = memo[i-1] + memo[i-2];
-        }       
+            memo[i] = memo[i-1] + memo[i-2];   
+            
         return memo[n];
 }
