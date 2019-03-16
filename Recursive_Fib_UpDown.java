a345cf13c4038a1c3dc4b0c467492510a5179f57
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2019-03-16 15:11:27 +0800

    Create Recursive_Fib_UpDown.java

---
+++ Recursive_Fib_UpDown.java
@@ -0,0 +1,23 @@
+public static int Fibonacci(int n)
+{
+        if(n <= 0)
+            return n;
+        int []memo = new int[n+1];        
+        for(int i = 0; i <= n; i++)
+            memo[i]=-1;
+        return fib(n, memo);
+}
+    
+public static int fib(int n,int []memo)
+{
+        if(memo[n] != -1)
+            return Memo[n];
+        //如果已经求出了fib(n)的值直接返回，否则将求出的值保存在memo备忘录中
+        
+        if(n<=2)
+            memo[n]=1;
+        else 
+            memo[n] = fib(n-1, memo) + fib(n-2, memo);  
+        
+        return memo[n];
+}
